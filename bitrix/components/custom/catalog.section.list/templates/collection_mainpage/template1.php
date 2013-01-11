e883deb6a421ccfbdc52a5712155ad8db032d5e3
Igor Shurshin <igor.shurshin@gmail.com>
2013-01-11 19:58:08 +0400

    MASSIVE cleanup!!!

--- bitrix/components/custom/catalog.section.list/templates/collection_mainpage/template1.php
+++
@@ -1,9 +0,0 @@
-<?
-if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-?><div class="left_menu"><?
-	$CURRENT_DEPTH=$arResult["SECTION"]["DEPTH_LEVEL"]+1;
-    foreach($arResult["SECTIONS"] as $arSection)
-	{
-    	?><div style="margin:0 0 0 <?=(($arSection["DEPTH_LEVEL"] - $CURRENT_DEPTH)  * 20)?>px;"><a href="<?=$arSection["SECTION_PAGE_URL"]?>"><h2 <?=($arResult["SECTION"]["DEPTH_LEVEL"] == 2)?' class="left_menu_b"':' class="left_menu"';?>><?=$arSection["NAME"]?></h2></a></div	><?
-	}
-?></div>
--- bitrix/components/custom2/catalog/templates/properties/lang/ru/.parameters.php
+++
@@ -1,6 +0,0 @@
-<?
-$MESS ['TP_BC_PRODUCT_QUANTITY_VARIABLE'] = "Ќазвание переменной, в которой передаетс€ количество товара";
-$MESS ['TP_BC_PRODUCT_PROPS_VARIABLE'] = "Ќазвание переменной, в которой передаютс€ характеристики товара";
-$MESS ['TP_BC_USE_PRODUCT_QUANTITY'] = "–азрешить указание количества товара";
-$MESS ['TP_BC_PRODUCT_PROPERTIES'] = "’арактеристики товара";
-?>
