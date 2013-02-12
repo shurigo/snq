85971285af3199b7f8f0d21fbc5bfc2d94ac6c16
Igor Shurshin <igor.shurshin@gmail.com>
2013-02-12 20:08:44 +0400

    UI: available cities list

--- inc/header.php
+++ inc/header.php
@@ -1,9 +1,10 @@
-    <header class="header">
+<? include($_SERVER['DOCUMENT_ROOT'].'/ipgeo/geohelper.php');?> 
+	<header class="header">
       <div class="logo"><a href="/"><img src="/images/logo.png" width="200" height="57" alt="Снежная Королева"></a></div>
       <!-- end .logo-->
       <div class="phone">8(800) 777-8-999</div>
       <!-- end .phone-->
-      <nav class="menu1"><select name="city" class=""><option value="0">Moscow</option></select> <span>|</span><a href="/our_shops/" rel="nofollow">Магазины</a> <span>|</span> <a href="/about/contacts/" rel="nofollow">Контакты</a> <span>|</span> <a href="/about/about_fur/">Интересное о мехе</a></nav>
+	  <nav class="menu1"><select name="city" class=""><? print_city_option_html(); ?></select> <span>|</span><a href="/our_shops/" rel="nofollow">Магазины</a> <span>|</span> <a href="/about/contacts/" rel="nofollow">Контакты</a> <span>|</span> <a href="/about/about_fur/">Интересное о мехе</a></nav>
       <!-- end .menu1-->
     </header>
     <!-- end .header-->
---
+++ ipgeo/geohelper.php
@@ -0,0 +1,85 @@
+<?
+  require_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php'); 
+  require_once($_SERVER['DOCUMENT_ROOT'].'/ipgeo/geo.php'); 
+
+  function get_my_city () {
+//	if(isset($_COOKIE['city'])) return $_COOKIE['city'];
+    $res_default = implode(' ', Array(2097, '??????'));
+	if(!CModule::IncludeModule('iblock')) return $res_default;
+	$geo = new Geo(); 
+	$ip = $geo->get_ip();
+	$ip = '95.24.22.7';// ??????
+	$ip = '195.224.222.227'; // ?? ??????
+	$ip = '217.199.218.0'; // ?????
+	$ip = '217.149.188.152'; // ?????????
+	if(empty($ip)) return $res_default;
+	$ip_long = ip2long($ip);
+	// convert negative values to positive
+	if($ip_long < 0) $ip_long = 4294967296 + $ip_long;
+	$filter = Array(
+	  'IBLOCK_ID' => 14,
+	  'IBLOCK_ACTIVE' => 'Y',
+	  'ACTIVE_DATE' => 'Y',
+	  'ACTIVE' => 'Y',
+	  'CHECK_PERMISSIONS' => 'Y',
+	  'ACTIVE' => 'Y',
+	  'PROPERTY' => Array('<=col_range_start' => $ip_long, '>=col_range_end' => $ip_long)
+	);
+	$select = Array('ID', 'NAME', 'IBLOCK_ID');
+	// find a city by IP range
+	$elements = CIBlockElement::GetList(
+	  Array(),
+	  $filter, 
+	  false,
+	  false,
+	  $select
+	);
+	$element = $elements->GetNextElement();
+	if(!$element) return $res_default;
+	$props = $element->GetProperties();
+	$city_id = $props['col_city_id']['VALUE'];
+	if(empty($city_id) || !is_numeric($city_id)) return $res_default;		
+	$filter = Array(
+	  'IBLOCK_ID' => 13,
+	  'ACTIVE' => 'Y',
+	  'XML_ID' => IntVal($city_id));
+	$select = Array('ID', 'NAME', 'IBLOCK_ID');
+	$elements = CIBlockElement::GetList(
+	  Array(),
+	  $filter,
+	  false,
+	  false,
+	  array()
+	);
+    $element = $elements->GetNextElement();
+	if(!$element) return $res_default;
+	$fields = $element->GetFields();
+	$ret = implode(' ', Array($fields['XML_ID'], $fields['NAME']));
+	setcookie('city', $ret, time() + 60 * 60 * 24 * 30);
+
+	return $ret;
+  }
+	
+  function get_available_cities() {
+    if(!CModule::IncludeModule('iblock')) return Array();
+    $filter = Array(
+      'IBLOCK_ID' => 7, //our shops
+       'ACTIVE' => 'Y',
+    );
+    $sort = Array('NAME' => 'ASC');
+    $sections = CIBlockSection::GetList($sort, $filter, false, array(), false);
+    $ret = Array();
+    while($section = $sections->GetNext()) {
+      //$ret[$section['NAME']] = $section['ID'];
+      $ret[] = Array($section['NAME'], $section['ID']);
+    }
+    return $ret;
+  }
+
+  function print_city_option_html() {
+	$cities = get_available_cities();
+   	foreach($cities as $c) {
+	  echo '<option value="'.$c[1].'"'.($c[0] == $city[1] ? 'selected="selected"':'').'>'.$c[0].'</option>';
+	} 
+  }
+?>
