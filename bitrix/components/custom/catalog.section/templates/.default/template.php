9446c56c6ca7d6a4624d11b7d832b3fada189162
levivya <vyacheslav.levin@gmail.com>
2013-03-04 19:25:18 +0400

    fixed the lates issue with half loading for catalog page

--- bitrix/components/custom/catalog.section/templates/.default/template.php
+++ bitrix/components/custom/catalog.section/templates/.default/template.php
@@ -1,39 +1,31 @@
 <?
-if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-$url_array = explode("/", $APPLICATION->GetCurPage());
-?><?
-if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-$url_array = explode("/", $APPLICATION->GetCurPage());
+  if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+  $url_array = explode("/", $APPLICATION->GetCurPage());
 ?>
-
 <section class="mainContent">
 <?
-	$APPLICATION->IncludeComponent(
-		"bitrix:breadcrumb",
-		"breadcrumb",
-		Array(
-			"START_FROM" => "1",
-			"PATH" => "",
-			"SITE_ID" => "-"
-		),
-		false
-	);
+$APPLICATION->IncludeComponent(
+"bitrix:breadcrumb",
+"breadcrumb",
+Array(
+"START_FROM" => "1",
+"PATH" => "",
+"SITE_ID" => "-"
+),
+false
+);
 ?>
-
-
 <input type="hidden" id="section" value="<?=$url_array[2]?>">
 <?if($arParams['JSON'] == "1"):?>
 <?echo '{
-  "data": {
-    "next": true,
-    "html":"';
+"data": {
+"next": true,
+"html":"';
 ?>
 <?endif;?>
 
 <section class="catalog">
-
 <?foreach($arResult["ITEMS"] as $arElement):?>
-
 <?
  //image resizing
  $resizer = $arElement['DETAIL_PICTURE'];
@@ -41,34 +33,34 @@ $url_array = explode("/", $APPLICATION->GetCurPage());
  $img = $file['src'];
 ?>
 
-	<article>
-		<a href="<?=$arElement["DETAIL_PAGE_URL"]?>">
-          <span class="photo"><span class="cell"><!--[if lte IE 7]><span><span><![endif]-->
-          <img src="<?=$img?>" alt="<?=$arElement['NAME']?>"> <!--[if lte IE 7]></span></span><![endif]--></span></span> <!-- end .photo-->
+<article>
+<a href="<?=$arElement["DETAIL_PAGE_URL"]?>">
+<span class="photo"><span class="cell"><!--[if lte IE 7]><span><span><![endif]-->
+<img src="<?=$img?>" alt="<?=$arElement['NAME']?>"> <!--[if lte IE 7]></span></span><![endif]--></span></span> <!-- end .photo-->
 
-			<span class="text">
-				<span class="name">
-				<?=strip_tags($arElement['DISPLAY_PROPERTIES']['col_brand']['DISPLAY_VALUE']);?>
-				</span>
-				<?=$arElement['NAME']?>
-			</span>
-			<!-- end .text -->
-			<?if(!empty($arElement['PROPERTIES']['col_price_new']['VALUE'])):?>
-			<span class="price bg-red">
-				<?=number_format($arElement['PROPERTIES']['col_price_new']['VALUE'], 0, '.', ' ').' руб.';?>
-				<del>
-					<?=number_format($arElement['PROPERTIES']['col_price']['VALUE'], 0, '.', ' ').' руб.';?>
-				</del>
-			<?else:?>
-			<span class="price">
-				<?=number_format($arElement['PROPERTIES']['col_price']['VALUE'], 0, '.', ' ').' руб.';?>
-				<new>New</new>
-			<?endif?>
-			</span>
-      <!-- end .price-->
-		</a>
-	</article>
-	<!-- end .article -->
+<span class="text">
+<span class="name">
+<?=strip_tags($arElement['DISPLAY_PROPERTIES']['col_brand']['DISPLAY_VALUE']);?>
+</span>
+<?=$arElement['NAME']?>
+</span>
+<!-- end .text -->
+<?if(!empty($arElement['PROPERTIES']['col_price_new']['VALUE'])):?>
+<span class="price bg-red">
+<?=number_format($arElement['PROPERTIES']['col_price_new']['VALUE'], 0, '.', ' ').' Руб.';?>
+<del>
+<?=number_format($arElement['PROPERTIES']['col_price']['VALUE'], 0, '.', ' ').' Руб.';?>
+</del>
+<?else:?>
+<span class="price">
+<?=number_format($arElement['PROPERTIES']['col_price']['VALUE'], 0, '.', ' ').' Руб.';?>
+<new>New</new>
+<?endif?>
+</span>
+<!-- end .price-->
+</a>
+</article>
+<!-- end .article -->
 
 <?endforeach; // foreach($arResult["ITEMS"] as $arElement):?>
 <?if($arParams['JSON'] == "1"):?>
