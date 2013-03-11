f348d08cb439b20be345586b906fb610b7a9e027
Igor Shurshin <igor.shurshin@gmail.com>
2013-03-11 18:15:37 +0400

    Update init.php
    minor code improvement

--- bitrix/php_interface/init.php
+++ bitrix/php_interface/init.php
@@ -5,28 +5,32 @@
 
   function Prefix_FunctionName() {
     global $APPLICATION;
-		$path = explode('/', $APPLICATION->GetCurPage());
-		if($APPLICATION->GetCurPage() === '/services/coupon/') {
-			LocalRedirect('/404.php', true, '404 Not Found');
-		}
-		while ("" === end($path))
-		{
-			array_pop($path);
-		}
-		array_pop($path);
-		$redirect_url = '/404.php';
-		if(!empty($path)) {
-			$redirect_url = implode('/', $path).'/';
-		}
+    $path = explode('/', $APPLICATION->GetCurPage());
+    if($APPLICATION->GetCurPage() === '/services/coupon/') {
+      LocalRedirect('/404.php', true, '404 Not Found');
+      exit();
+    }
+		
+    while ("" === end($path)) { 
+      array_pop($path); 
+    }
+
+    array_pop($path);
+    $redirect_url = '/404.php';
+    if(!empty($path)) {
+      $redirect_url = implode('/', $path).'/';
+    }
+
     // Check if we need to show the content of the 404 page
     if (!defined('ERROR_404') || ERROR_404 != 'Y') {
-        return;
+      return;
     }
+    
     // Display the 404 page unless it is already being displayed
     if ($APPLICATION->GetCurPage() != PREFIX_PATH_404) {
-        LocalRedirect($redirect_url, false, '301 Moved permanently');
-        header('X-Accel-Redirect: '.$redirect_url);
-        exit();
+      LocalRedirect($redirect_url, false, '301 Moved permanently');
+      header('X-Accel-Redirect: '.$redirect_url);
+      exit();
     }
 }
 ?>
