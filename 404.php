c6a409a6a4fab469d9d1ce06955a1979866f4a82
levivya <vyacheslav.levin@gmail.com>
2013-03-11 10:11:21 +0400

    minor change in 404 page

--- 404.php
+++ 404.php
@@ -2,14 +2,14 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Ошибка 404");
 ?>
-<div> 
-  <table style="width: 100%;"> 
-    <tbody> 
-      <tr> <td valign="top" style="width: auto; padding: 0px 29px 0pt 16px;"> 
+<div>
+  <table style="width: 100%;">
+    <tbody>
+      <tr> <td valign="top" style="width: auto; padding: 0px 29px 0pt 16px;">
           <h1>Ошибка 404. Страница не найдена.</h1>
-        
-          <p>Вы можете перейти в раздел "<a href="/collection/">Коллекция</a>", "<a href="/actions/">Акции</a>" или перейти на <a href="/">главную страницу</a> сайта. 
-         
+
+          <p>Вы можете перейти в раздел "<a href="/collection/woman/">Коллекция</a>", "<a href="/actions/">Акции</a>" или перейти на <a href="/">главную страницу</a> сайта.
+
 
          </td> <td valign="top" style="width: 237px; padding: 30px 0pt 0pt;"></td> </tr>
      </tbody>
