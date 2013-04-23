3097ae99bcf5e74402510f6354b67c94e451f00d
shurigo <igor.shurshin@gmail.com>
2013-04-24 01:32:49 +0400

    do not display the catalog when no sections selected

--- bitrix/templates/inside_pages/components/bitrix/news/actions_active/bitrix/news.detail/.default/template.php
+++ bitrix/templates/inside_pages/components/bitrix/news/actions_active/bitrix/news.detail/.default/template.php
@@ -8,7 +8,6 @@
 <?else:?>
 		<p itemprop="text"><?echo $arResult["PREVIEW_TEXT"];?></p>
 <?endif?>
-<h1>Вещи, участвующие в акции</h1>
 <?
 	$action_catalog_filter = Array(
 		'IBLOCK_ID' => '1',
@@ -25,84 +24,87 @@
 	$_SESSION['action_catalog_filter'] = $action_catalog_filter;
 	$GLOBALS['discount_only'] = $discount_only;
 	$_SESSION['discount_only'] = $discount_only;
-	require($_SERVER['DOCUMENT_ROOT'].'/collection/init.php');
-
-	$APPLICATION->IncludeComponent(
-		"custom:catalog.section",
-		"",
-		Array(
-				"AJAX_MODE" => "N",
-				"SEF_MODE" => "Y",
-				"IBLOCK_TYPE" => "collection",
-				"IBLOCK_ID" => "1",
-				"USE_FILTER" => "Y",
-				"FILTER_NAME" => "action_catalog_filter",
-				"INCLUDE_BRANDS" => "N",
-				"USE_REVIEW" => "N",
-				"USE_COMPARE" => "N",
-				"USE_SORT" => "N",
-				"ACTIONS_MODE" => "Y",
-				"DISCOUNT_ONLY" => $discount_only,
-				"BY_LINK" => "Y",
-				"SHOW_TOP_ELEMENTS" => "N",
-				"PAGE_ELEMENT_COUNT" => "32",
-				"LINE_ELEMENT_COUNT" => "4",
-				"ELEMENT_SORT_FIELD" => 'sort',
-				"ELEMENT_SORT_ORDER" => 'asc',
-				"PROPERTY_CODE" => array(0=>"col_model_code",1=>"col_price",2=>"col_sizes",3=>"col_brand",4=>"col_price_origin",5=>"add_pic_1",6=>"add_pic_2"),
-				"INCLUDE_SUBSECTIONS" => "Y",
-				"LIST_META_KEYWORDS" => "UF_SEC_KEYWORDS",
-				"LIST_META_DESCRIPTION" => "UF_SEC_DESCRIPTON",
-				"LIST_BROWSER_TITLE" => "UF_SEC_TITLE",
-				"PROPERTY_CODE" => array(0=>"col_model_code",1=>"col_price",2=>"col_sizes",3=>"col_brand",4=>"col_price_origin",5=>"add_pic_1",6=>"add_pic_2"),
-				"DETAIL_META_KEYWORDS" => "col_keywords",
-				"DETAIL_META_DESCRIPTION" => "col_description",
-				"DETAIL_BROWSER_TITLE" => "col_title",
-				"ACTION_VARIABLE" => "action",
-				"PRODUCT_ID_VARIABLE" => "id",
-				"SECTION_ID_VARIABLE" => "SECTION_ID",
-				"DISPLAY_PANEL" => "N",
-				"CACHE_TYPE" => "N",
-				"CACHE_TIME" => "3600",
-				"CACHE_FILTER" => "N",
-				"CACHE_GROUPS" => "Y",
-				"SET_TITLE" => "Y",
-				"SET_STATUS_404" => "Y",
-				"PRICE_CODE" => array(0=>"col_price",),
-				"USE_PRICE_COUNT" => "N",
-				"SHOW_PRICE_COUNT" => "1",
-				"PRICE_VAT_INCLUDE" => "Y",
-				"PRICE_VAT_SHOW_VALUE" => "N",
-				"LINK_IBLOCK_TYPE" => "",
-				"LINK_IBLOCK_ID" => "",
-				"LINK_PROPERTY_SID" => "",
-				"LINK_ELEMENTS_URL" => "link.php?PARENT_ELEMENT_ID=#ELEMENT_ID#",
-				"DISPLAY_TOP_PAGER" => "N",
-				"DISPLAY_BOTTOM_PAGER" => "N",
-				"DISPLAY_ELEMENT_SELECT_BOX" => "N",
-				"ELEMENT_SORT_FIELD_BOX" => "id",
-				"ELEMENT_SORT_ORDER_BOX" => "asc",
-				"COMPARE_ELEMENT_SORT_FIELD" => "sort",
-				"COMPARE_ELEMENT_SORT_ORDER" => "asc",
-				"AJAX_OPTION_SHADOW" => "Y",
-				"AJAX_OPTION_JUMP" => "N",
-				"AJAX_OPTION_STYLE" => "Y",
-				"AJAX_OPTION_HISTORY" => "N",
-				"SEF_FOLDER" => "/collection/",
-				"SEF_URL_TEMPLATES" => Array(
-					"section" => "#SECTION_CODE#/",
-					"element" => "#SECTION_CODE#/#ELEMENT_ID#/",
-				),
-				"VARIABLE_ALIASES" => Array(
-					"section" => Array(),
-					"element" => Array(),
-				),
-				"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-				"ADD_SECTIONS_CHAIN" => "N",
-				"JSON" => "n" 
-			)
-		);
-?>
+	require($_SERVER['DOCUMENT_ROOT'].'/collection/init.php');?>
+	<?if(!empty($arResult['PROPERTIES']['col_sections']['VALUE'])):?>  
+		<br>
+		<h1>Вещи, участвующие в акции</h1>
+		<hr>
+		<?$APPLICATION->IncludeComponent(
+			"custom:catalog.section",
+			"",
+			Array(
+					"AJAX_MODE" => "N",
+					"SEF_MODE" => "Y",
+					"IBLOCK_TYPE" => "collection",
+					"IBLOCK_ID" => "1",
+					"USE_FILTER" => "Y",
+					"FILTER_NAME" => "action_catalog_filter",
+					"INCLUDE_BRANDS" => "N",
+					"USE_REVIEW" => "N",
+					"USE_COMPARE" => "N",
+					"USE_SORT" => "N",
+					"ACTIONS_MODE" => "Y",
+					"DISCOUNT_ONLY" => $discount_only,
+					"BY_LINK" => "Y",
+					"SHOW_TOP_ELEMENTS" => "N",
+					"PAGE_ELEMENT_COUNT" => "32",
+					"LINE_ELEMENT_COUNT" => "4",
+					"ELEMENT_SORT_FIELD" => 'sort',
+					"ELEMENT_SORT_ORDER" => 'asc',
+					"PROPERTY_CODE" => array(0=>"col_model_code",1=>"col_price",2=>"col_sizes",3=>"col_brand",4=>"col_price_origin",5=>"add_pic_1",6=>"add_pic_2"),
+					"INCLUDE_SUBSECTIONS" => "Y",
+					"LIST_META_KEYWORDS" => "UF_SEC_KEYWORDS",
+					"LIST_META_DESCRIPTION" => "UF_SEC_DESCRIPTON",
+					"LIST_BROWSER_TITLE" => "UF_SEC_TITLE",
+					"PROPERTY_CODE" => array(0=>"col_model_code",1=>"col_price",2=>"col_sizes",3=>"col_brand",4=>"col_price_origin",5=>"add_pic_1",6=>"add_pic_2"),
+					"DETAIL_META_KEYWORDS" => "col_keywords",
+					"DETAIL_META_DESCRIPTION" => "col_description",
+					"DETAIL_BROWSER_TITLE" => "col_title",
+					"ACTION_VARIABLE" => "action",
+					"PRODUCT_ID_VARIABLE" => "id",
+					"SECTION_ID_VARIABLE" => "SECTION_ID",
+					"DISPLAY_PANEL" => "N",
+					"CACHE_TYPE" => "N",
+					"CACHE_TIME" => "3600",
+					"CACHE_FILTER" => "N",
+					"CACHE_GROUPS" => "Y",
+					"SET_TITLE" => "Y",
+					"SET_STATUS_404" => "Y",
+					"PRICE_CODE" => array(0=>"col_price",),
+					"USE_PRICE_COUNT" => "N",
+					"SHOW_PRICE_COUNT" => "1",
+					"PRICE_VAT_INCLUDE" => "Y",
+					"PRICE_VAT_SHOW_VALUE" => "N",
+					"LINK_IBLOCK_TYPE" => "",
+					"LINK_IBLOCK_ID" => "",
+					"LINK_PROPERTY_SID" => "",
+					"LINK_ELEMENTS_URL" => "link.php?PARENT_ELEMENT_ID=#ELEMENT_ID#",
+					"DISPLAY_TOP_PAGER" => "N",
+					"DISPLAY_BOTTOM_PAGER" => "N",
+					"DISPLAY_ELEMENT_SELECT_BOX" => "N",
+					"ELEMENT_SORT_FIELD_BOX" => "id",
+					"ELEMENT_SORT_ORDER_BOX" => "asc",
+					"COMPARE_ELEMENT_SORT_FIELD" => "sort",
+					"COMPARE_ELEMENT_SORT_ORDER" => "asc",
+					"AJAX_OPTION_SHADOW" => "Y",
+					"AJAX_OPTION_JUMP" => "N",
+					"AJAX_OPTION_STYLE" => "Y",
+					"AJAX_OPTION_HISTORY" => "N",
+					"SEF_FOLDER" => "/collection/",
+					"SEF_URL_TEMPLATES" => Array(
+						"section" => "#SECTION_CODE#/",
+						"element" => "#SECTION_CODE#/#ELEMENT_ID#/",
+					),
+					"VARIABLE_ALIASES" => Array(
+						"section" => Array(),
+						"element" => Array(),
+					),
+					"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+					"ADD_SECTIONS_CHAIN" => "N",
+					"JSON" => "n" 
+				)
+		);?>
+	<?endif;// if(!empty($arResult['PROPERTIES']['col_sections']['VALUE'])) ?>
 <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
 		<img  itemprop="image" border="0" src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>" width="777px"  alt="<?=$arResult["NAME"]?>"  title="<?=$arResult["NAME"]?>" />
 <?endif?>
