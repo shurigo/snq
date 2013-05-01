5faa3b3de1570c506b5c9c0664d17e6b06633abf
Slava Levin <vyacheslav.levin@gmail.com>
2013-05-01 10:42:13 +0400

    work on vacancies

--- about/vacancies/detail.php
+++ about/vacancies/detail.php
@@ -1,18 +1,24 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php"); ?>
 
+<table>
+<tr><td><a href="office.php">Вакансии в офисе</a></td><td><a href="shops.php">Вакансии в магазинах</a></td><td>Вакансии Логистического центра</td></tr>
+</table><br>
 
 <?
-//echo "vacancy_id=".$id;
-
 //get element
-$arSelect = Array("ID","NAME","PREVIEW_TEXT");
+$arSelect = Array("ID","NAME","PREVIEW_TEXT","PROPERTY_col_email");
 $arFilter = Array("ID"=>$id);
 $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
 $ob = $res->GetNextElement();
 $arFields = $ob->GetFields();
 
+//print_r($arFields);
+
+//print
 echo "<h1>".$arFields['NAME']."</h1>";
-echo "<div>".$arFields['PREVIEW_TEXT']."</div>";
+echo $arFields['PREVIEW_TEXT'];
+echo "<br><div><b>E-mail:</b> ".$arFields['PROPERTY_COL_EMAIL_VALUE']."</div>";
+
 ?>
 
 
