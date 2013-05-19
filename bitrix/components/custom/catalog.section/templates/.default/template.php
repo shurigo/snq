083ad094c5662679a5404ffe4c31694a395dbdb2
shurigo <igor.shurshin@gmail.com>
2013-05-20 00:14:59 +0400

    collection view/filter fix

--- bitrix/components/custom/catalog.section/templates/.default/template.php
+++ bitrix/components/custom/catalog.section/templates/.default/template.php
@@ -38,7 +38,7 @@
 	<?elseif($arParams['VIEW_MODE'] == 'brands'):?>
 		<section class="catalog" data-page="<?='/collection/?m=a&BRAND_ID='.$_GET['BRAND_ID'];?>">
 	<?else:?>
-		<section class="catalog" data-page="/collection/">
+		<section class="catalog" data-page="<?=$APPLICATION->GetCurPage();?>">
 	<?endif;?>
 <?endif; //end normal page?>
 <?
