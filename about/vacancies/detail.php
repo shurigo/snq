014f93c16590241eb77b77464c394557c79c221d
Slava Levin <vyacheslav.levin@gmail.com>
2013-05-26 20:04:41 +0400

    vacancies detail page minor amendment

--- about/vacancies/detail.php
+++ about/vacancies/detail.php
@@ -6,7 +6,7 @@
 
 <?
 //get element
-$arSelect = Array("ID","NAME","PREVIEW_TEXT","PROPERTY_col_email");
+$arSelect = Array("ID","NAME","PREVIEW_TEXT","PROPERTY_col_email","PROPERTY_col_phone");
 $arFilter = Array("ID"=>$id);
 $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
 $ob = $res->GetNextElement();
@@ -17,7 +17,8 @@ $arFields = $ob->GetFields();
 //print
 echo "<h1>".$arFields['NAME']."</h1>";
 echo $arFields['PREVIEW_TEXT'];
-echo "<br><div><b>E-mail:</b> ".$arFields['PROPERTY_COL_EMAIL_VALUE']."</div>";
+if (strlen($arFields['PROPERTY_COL_PHONE_VALUE'])>0) echo '<br><div><b>Контактный телефон:</b> '.$arFields['PROPERTY_COL_PHONE_VALUE'].'</div>';
+if (strlen($arFields['PROPERTY_COL_EMAIL_VALUE'])>0) echo '<br><div><b>E-mail:</b> <a href="mailto:'.$arFields['PROPERTY_COL_EMAIL_VALUE'].'">'.$arFields['PROPERTY_COL_EMAIL_VALUE'].'</a></div>';
 
 ?>
 
