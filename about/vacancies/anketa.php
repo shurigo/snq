a388cb95314cff25681fd72d09fc77cb67c7689a
Slava Levin <vyacheslav.levin@gmail.com>
2013-05-28 23:48:29 +0400

    vacancy anketa - initial commit

---
+++ about/vacancies/anketa.php
@@ -0,0 +1,20 @@
+<br>
+<hr>
+<h1>Анкета</h1>
+<form method="post">
+<fieldset>
+<table>
+<tr><td width="30%">Фамилия:*</td><td width="70%"><input type="text"   name="last_name" size="40" maxlength="40"></td></tr>
+<tr><td width="30%">Имя:*</td><td width="70%"><input type="text"   name="first_name" size="40" maxlength="40"></td></tr>
+<tr><td width="30%">Отчество:*</td><td width="70%"><input type="text"   name="middle_name" size="40" maxlength="40"></td></tr>
+<tr><td width="30%">Дата рождения:*</td><td width="70%"><input type="text"   name="middle_name" size="40" maxlength="40"></td></tr>
+<tr><td width="30%">Гражданство:*</td><td width="70%"><input type="text"   name="middle_name" size="40" maxlength="40"></td></tr>
+<tr><td width="30%">Адрес фактического проживания:</td><td width="70%"><input type="text"   name="middle_name" size="40" maxlength="40"></td></tr>
+<tr><td width="30%">Контактные телефоны:*</td><td width="70%">мобильный: +7<input type="text"   name="mobile_phone" size="10" maxlength="40">&nbsp;или домашний: +7<input type="text"   name="home_phone" size="10" maxlength="40"></td></tr>
+<tr><td width="30%">Email:</td><td width="70%"><input type="text"   name="email" size="40" maxlength="40"></td></tr>
+
+<tr><td><br />&nbsp;</td><td><input type="submit" name="send" value="отправить"></td></tr>
+</table>
+</fieldset>
+</form>
+</table>
