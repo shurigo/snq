bc55edbec86e9c1fbc4e32c27992877cccaadbba
Slava Levin <vyacheslav.levin@gmail.com>
2013-07-02 23:20:10 +0400

    change <strong> to <h1> in navigation string for seo

--- bitrix/templates/inside_pages/components/bitrix/breadcrumb/breadcrumb/template.php
+++ bitrix/templates/inside_pages/components/bitrix/breadcrumb/breadcrumb/template.php
@@ -14,7 +14,7 @@
 	  if($arResult[$index]["LINK"] <> CMain::GetCurDir())
 		$strReturn .= '<a href="'.$arResult[$index]["LINK"].'" title="'.$title.'" >'.$title.'</a>';
 	  else
-		$strReturn .= '<strong>'.$title.'</strong>';
+		$strReturn .= '<h1 class="nav">'.$title.'</h1>';
   }
 
   $strReturn = "<nav class=\"path\">".$strReturn."</nav> <!-- end .path-->";
