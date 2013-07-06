64c23c3366ec59322080376db54ad4969381d035
Slava Levin <vyacheslav.levin@gmail.com>
2013-07-06 12:49:13 +0400

    add new pixels

--- bitrix/templates/main_page/footer.php
+++ bitrix/templates/main_page/footer.php
@@ -157,5 +157,19 @@ screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
 <noscript><div><img src="//mc.yandex.ru/watch/7080487" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 
+<!--  Realtime Targeting tracker - httpool Yuri begin -->
+<script type="text/javascript">
+var _agTrack = _agTrack || [];
+_agTrack.push({ cid:"D40GD" });
+(function(d) { try {
+    var scr = d.getElementsByTagName("script")[0], f = d.createElement('iframe');
+    f.src = "javascript:''"; f.style.display = "none"; scr.parentNode.insertBefore(f, scr); f.contentWindow.name = "ag:scrf";
+    var d2 = f.contentWindow.document,fn='write';
+    d2.open()[fn]("<body onload=\"var d=document,s=d.createElement('script');s.id='ag:trackscript.1';s.src='//track.idtargeting.com/D40GD/track.js';d.body.appendChild(s);\">");
+    d2.close();
+}catch(e){}})(document);
+</script>
+<!-- Realtime Targeting tracker - httpool Yuri end -->
+
 </body>
 </html>
