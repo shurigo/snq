813b980b00b8a880e78dc82f63ded752237b59d7
Slava Levin <vyacheslav.levin@gmail.com>
2013-07-06 13:49:27 +0400

    seo for furs change title

--- inc/header.php
+++ inc/header.php
@@ -11,7 +11,7 @@
       <div class="city"><select id="city-select" class="customSelect"><? print_city_option_html();?></select></div>
       <div class="phone">8(800) 777-8-999</div>
       <!-- end .phone-->
-	  <nav class="menu1"><a href="/our_shops/" rel="nofollow" onClick="trackOutboundLink(this, 'Outbound Links', 'our_shops'); return false;">Магазины</a> <span>|</span> <a href="/about/contacts/" rel="nofollow">Контакты</a> <span>|</span> <a href="/about/about_fur/">Интересное о мехе</a></nav>
+	  <nav class="menu1"><a href="/our_shops/" rel="nofollow" onClick="trackOutboundLink(this, 'Outbound Links', 'our_shops'); return false;">Магазины</a> <span>|</span> <a href="/about/contacts/" rel="nofollow">Контакты</a> <span>|</span> <a href="/about/about_fur/">Меха от А до Я</a></nav>
       <!-- end .menu1-->
     </header>
     <!-- end .header-->
