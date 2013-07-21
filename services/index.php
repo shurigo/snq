8342b5f10494b6d77e7a4464f22fd133eb2aa490
Slava Levin <vyacheslav.levin@gmail.com>
2013-07-21 20:37:17 +0400

    minor design update at service page

--- services/index.php
+++ services/index.php
@@ -2,21 +2,19 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Услуги");
 ?>
-<section class="mainContent2">
-
 <h1>Услуги</h1>
 
-        <table width="100%" height="150px">
-            <tr>
-                <td><a href="/services/credit/">Потребительское кредитование</a></td>
-                <td><a href="/services/discount/">Дисконтная программа</a></td>
+        <table width="100%" height="450px" border="1" bordercolor="white" bgcolor="#11acdc">
+            <tr align="center">
+                <td width="50%"><a href="/services/credit/" style="font-size:22px;color:white;text-decoration: none;">Потребительское кредитование</a></td>
+                <td width="50%"><a href="/services/discount/" style="font-size:22px;color:white;text-decoration: none;">Дисконтная программа</a></td>
 
             </tr>
-            <tr>
-                <td><a href="/services/cards/">Подарочные карты</a></td>
-                <td><a href="/services/megacard/">Мы принимаем к оплате MEGACARD</a></td>
+            <tr align="center">
+                <td width="50%"><a href="/services/cards/" style="font-size:22px;color:white;text-decoration: none;">Подарочные карты</a></td>
+                <td width="50%"><a href="/services/megacard/" style="font-size:22px;color:white;text-decoration: none;">Мы принимаем к оплате MEGACARD</a></td>
             </tr>
         </table>
-</section>
+
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
