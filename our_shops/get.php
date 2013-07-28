a085777b445738c7d6daf0630ee829f6b392b4dc
shurigo <igor.shurshin@gmail.com>
2013-07-28 12:52:46 +0400

    display shops by city and size selected

--- our_shops/get.php
+++ our_shops/get.php
@@ -18,26 +18,48 @@
 		$rem[$shop_id] = $quantity;
 	}
 	$our_shops_iblock_id = getIblockIdByName('our_shops', 'our_shops');
+	$sections = CIBlockSection::GetList(
+		array(),
+		array('IBLOCK_ID' => IntVal($our_shops_iblock_id), 'NAME' => $_SESSION['city_name']),
+		false,
+		array('ID', 'IBLOCK_ID', 'NAME')
+	);
+	if(!$sections) die();
+	$section_id = $sections->GetNext()['ID'];
+	error_log($section_id,0);
 ?>
-  <p>Наличие в магазинах</p>
-  <table>
-	<?foreach($rem as $kais_id => $quantity):?>
-	<?
-			$shops = CIBlockElement::GetList(
+	<?if(count($rem) > 0):?>
+    <?$items_found = false;?>
+		<p>Наличие в магазинах:</p>
+		<table>
+		<?foreach($rem as $kais_id => $quantity):?>
+		  <?$shops = CIBlockElement::GetList(
 				array(),
 				array(
 					'IBLOCK_ID' => IntVal($our_shops_iblock_id),
-					'PROPERTY_col_kais_id' => $kais_id
+					'PROPERTY_col_kais_id' => $kais_id,
+					'SECTION_ID' => $section_id
 				)
 			);
 			if($e = $shops->GetNextElement()):?>
 				<?if($fields = $e->GetFields()):?>
+					<?if(!$items_found):?>
+						<?$items_found = true;?>
+						<tr>
+              <td>Магазин</td>
+              <td>Количество</td>
+            </tr>
+          <?endif?>
 					<tr>
 						<td><?=$fields['NAME']?></td>
-            <td><?=$quantity?></td>
+						<td><?=$quantity?></td>
 					</tr>
 					<?$props = $e->GetProperties();?>
 				<?endif?>
-		<?endif?>
-	<?endforeach?>
-  </table>
+			<?endif?>
+		<?endforeach?>
+		</table>
+	 <?if(!$items_found):?>
+     <p>В магазинах вашего города данного размера нет. Выбирите другой размер или город.</p>
+   <?endif?>
+  <?endif?>
