e43a4e3cd327586b09fe253b7233f7d8b8d31163
Slava Levin <vyacheslav.levin@gmail.com>
2013-09-18 23:37:58 +0400

    add subscription at an item page

--- bitrix/components/custom/subscribe.form/templates/.default/template.php
+++ bitrix/components/custom/subscribe.form/templates/.default/template.php
@@ -1,5 +1,5 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<form class="subscr" action="subscription.php">
+<form class="subscr" action="/subscription.php">
 <fieldset>
             <span>Подпишитесь на наши новости и акции</span>
             <input type="text" name="sf_EMAIL" size="20" style="border:1px solid #11acdc;" value="введите e-mail" title="<?=GetMessage("subscr_form_email_title")?>" onclick="this.value='';" onfocus="this.select()" />
