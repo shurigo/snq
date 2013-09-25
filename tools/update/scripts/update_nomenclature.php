6514059fef3a474b6dbe84d17c8f2dbfff5850f3
shurigo <igor.shurshin@gmail.com>
2013-09-25 22:41:26 +0400

    data load tools slightly improved

---
+++ tools/update/scripts/update_nomenclature.php
@@ -0,0 +1,11 @@
+<?
+  require_once('updater.php');
+
+  $_SERVER = array();
+  $_SERVER['DOCUMENT_ROOT'] = realpath(dirname(__FILE__).'/../../..');
+  require_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php');
+
+  $nomenclature_loader = new NomenclatureLoader(NOMENCLATURE_UPLOAD_FILE, false);
+ 	$nomenclature_loader->init();
+	$nomenclature_loader->load();
+?>
--- tools/update/scripts/updater.php
+++ tools/update/scripts/updater.php
@@ -128,7 +128,7 @@
 		  return "FieldInfo[name=$name,column=$column,type=$type]";
 		}
 	}
-
+/*
 	class PriceLoader extends BaseLoader {
 
 		public function __construct($file_name) {
@@ -203,17 +203,17 @@
 			return false;
 		}
 	}
-  
-	class NomenclatureLoader extends BaseLoader {
+ */ 
+  class NomenclatureLoader extends BaseLoader {
 		const iblock_code = 'nomenclature';
 
 		public function __construct($file_name, $skip_first_row = false) {
 			parent::__construct($file_name, $skip_first_row);
 			$this->logger = Logger::getLogger(__CLASS__);
 			$this->logger->debug('ctor()');
-			$this->fields['IDMFC']  = new FieldInfo('IDMFC',  2, FieldInfo::NUMBER_TYPE);
-			$this->fields['ART_NO'] = new FieldInfo('ART_NO', 3, FieldInfo::NUMBER_TYPE);
-			$this->fields['SIZE']   = new FieldInfo('SIZE',   4, FieldInfo::STRING_TYPE);
+			$this->fields['IDMFC']  = new FieldInfo('IDMFC',  1, FieldInfo::NUMBER_TYPE);
+			$this->fields['ART_NO'] = new FieldInfo('ART_NO', 2, FieldInfo::NUMBER_TYPE);
+			$this->fields['SIZE']   = new FieldInfo('SIZE',   3, FieldInfo::STRING_TYPE);
 		}
 
 		public function init() {
@@ -279,9 +279,9 @@
 			parent::__construct($file_name, $skip_first_row);
 			$this->logger = Logger::getLogger(__CLASS__);
 			$this->logger->debug('ctor()');
-			$this->fields['ART_NO'] = new FieldInfo('ART_NO', 1, FieldInfo::NUMBER_TYPE);
-			$this->fields['SHOP_EXT_ID'] = new FieldInfo('SHOP_EXT_ID', 2, FieldInfo::NUMBER_TYPE);
-			$this->fields['QUANTITY']   = new FieldInfo('QUANTITY', 3, FieldInfo::STRING_TYPE);
+			$this->fields['ART_NO'] = new FieldInfo('ART_NO', 0, FieldInfo::NUMBER_TYPE);
+			$this->fields['SHOP_EXT_ID'] = new FieldInfo('SHOP_EXT_ID', 1, FieldInfo::NUMBER_TYPE);
+			$this->fields['QUANTITY']   = new FieldInfo('QUANTITY', 2, FieldInfo::STRING_TYPE);
 		}
 
 		public function init() {
@@ -349,7 +349,4 @@
 // 	$nomenclature_loader->init();
 //	$nomenclature_loader->load();
 
-//  $remainder_loader = new RemainderLoader('remainder.csv', true);
-//	$remainder_loader->init();
-//	$remainder_loader->load();
 ?>
