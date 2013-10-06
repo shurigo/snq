fde8b9357b15f894d3f0a9080c36c968709be066
shurigo <igor.shurshin@gmail.com>
2013-10-07 00:09:25 +0400

    hide left menu when searching

--- collection/index.php
+++ collection/index.php
@@ -89,112 +89,112 @@
 		&& (empty($url_array[2]) || strtolower($url_array[2]) !== 'q' || !is_numeric($url_array[2]))) {
     LocalRedirect('/collection/woman/', true);
 	}
+
 	$view_mode = (empty($name_filter_raw) ? '' : 'search');
 	$APPLICATION->IncludeComponent(
 		"custom:catalog",
 		"",
 		Array(
-				"AJAX_MODE" => "N",
-				"SEF_MODE" => "Y",
-				"IBLOCK_TYPE" => "collection",
-				"IBLOCK_ID" => "1",
-				"USE_FILTER" => "Y",
-				"FILTER_NAME" => "arFilter",
-				"INCLUDE_BRANDS" => "Y",
-				"SEARCH_QUERY" => $name_filter_raw,
-				"VIEW_MODE" => $view_mode,
-				"USE_REVIEW" => "N",
-				"USE_COMPARE" => "N",
-				"USE_SORT" => "Y",
-				"DISCOUNT_ONLY" => $_SESSION['discount_only'],
-				"NOT_SHOW_NAV_CHAIN" => "N",
-				"SHOW_TOP_ELEMENTS" => "N",
-				"PAGE_ELEMENT_COUNT" => "12",
-				"LINE_ELEMENT_COUNT" => "4",
-				"ELEMENT_SORT_FIELD" => $sort_field,
-				"ELEMENT_SORT_ORDER" => $sort_order,
-				"LIST_PROPERTY_CODE" => array(0=>"col_model_code",1=>"col_price",2=>"col_sizes",3=>"col_brand",4=>"col_price_origin",5=>"add_pic_1",6=>"add_pic_2"),
-				"INCLUDE_SUBSECTIONS" => "Y",
-				"LIST_META_KEYWORDS" => "UF_SEC_KEYWORDS",
-				"LIST_META_DESCRIPTION" => "UF_SEC_DESCRIPTON",
-				"LIST_BROWSER_TITLE" => "UF_SEC_TITLE",
-				"DETAIL_PROPERTY_CODE" => array(0=>"col_model_code",1=>"col_price",2=>"col_sizes",3=>"col_brand",4=>"col_price_origin",5=>"add_pic_1",6=>"add_pic_2",7=>"col_im_link"),
-				"DETAIL_META_KEYWORDS" => "col_keywords",
-				"DETAIL_META_DESCRIPTION" => "col_description",
-				"DETAIL_BROWSER_TITLE" => "col_title",
-				"ACTION_VARIABLE" => "action",
-				"PRODUCT_ID_VARIABLE" => "id",
-				"SECTION_ID_VARIABLE" => "SECTION_ID",
-				"DISPLAY_PANEL" => "N",
-				"CACHE_TYPE" => "N",
-				"CACHE_TIME" => "3600",
-				"CACHE_FILTER" => "N",
-				"CACHE_GROUPS" => "Y",
-				"SET_TITLE" => "Y",
-				"SET_STATUS_404" => "Y",
-				"PRICE_CODE" => array(0=>"col_price",),
-				"USE_PRICE_COUNT" => "N",
-				"SHOW_PRICE_COUNT" => "1",
-				"PRICE_VAT_INCLUDE" => "Y",
-				"PRICE_VAT_SHOW_VALUE" => "N",
-				"LINK_IBLOCK_TYPE" => "",
-				"LINK_IBLOCK_ID" => "",
-				"LINK_PROPERTY_SID" => "",
-				"LINK_ELEMENTS_URL" => "link.php?PARENT_ELEMENT_ID=#ELEMENT_ID#",
-				"DISPLAY_TOP_PAGER" => "N",
-				"DISPLAY_BOTTOM_PAGER" => "N",
-				"DISPLAY_ELEMENT_SELECT_BOX" => "N",
-				"ELEMENT_SORT_FIELD_BOX" => "id",
-				"ELEMENT_SORT_ORDER_BOX" => "asc",
-				"AJAX_OPTION_SHADOW" => "Y",
-				"AJAX_OPTION_JUMP" => "N",
-				"AJAX_OPTION_STYLE" => "Y",
-				"AJAX_OPTION_HISTORY" => "N",
-				"SEF_FOLDER" => "/collection/",
-				"SEF_URL_TEMPLATES" => Array(
-					"section" => "#SECTION_CODE#/",
-					"element" => "#SECTION_CODE#/#ELEMENT_ID#/",
-				),
-				"VARIABLE_ALIASES" => Array(
-					"section" => Array(),
-					"element" => Array(),
-				),
-				"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-				"ADD_SECTIONS_CHAIN" => "N",
-				"JSON" => $json
+			"AJAX_MODE" => "N",
+			"SEF_MODE" => "Y",
+			"IBLOCK_TYPE" => "collection",
+			"IBLOCK_ID" => "1",
+			"USE_FILTER" => "Y",
+			"FILTER_NAME" => "arFilter",
+			"INCLUDE_BRANDS" => "Y",
+			"SEARCH_QUERY" => $name_filter_raw,
+			"VIEW_MODE" => $view_mode,
+			"USE_REVIEW" => "N",
+			"USE_COMPARE" => "N",
+			"USE_SORT" => "Y",
+			"DISCOUNT_ONLY" => $_SESSION['discount_only'],
+			"NOT_SHOW_NAV_CHAIN" => "N",
+			"SHOW_TOP_ELEMENTS" => "N",
+			"PAGE_ELEMENT_COUNT" => "12",
+			"LINE_ELEMENT_COUNT" => "4",
+			"ELEMENT_SORT_FIELD" => $sort_field,
+			"ELEMENT_SORT_ORDER" => $sort_order,
+			"LIST_PROPERTY_CODE" => array(0=>"col_model_code",1=>"col_price",2=>"col_sizes",3=>"col_brand",4=>"col_price_origin",5=>"add_pic_1",6=>"add_pic_2"),
+			"INCLUDE_SUBSECTIONS" => "Y",
+			"LIST_META_KEYWORDS" => "UF_SEC_KEYWORDS",
+			"LIST_META_DESCRIPTION" => "UF_SEC_DESCRIPTON",
+			"LIST_BROWSER_TITLE" => "UF_SEC_TITLE",
+			"DETAIL_PROPERTY_CODE" => array(0=>"col_model_code",1=>"col_price",2=>"col_sizes",3=>"col_brand",4=>"col_price_origin",5=>"add_pic_1",6=>"add_pic_2",7=>"col_im_link"),
+			"DETAIL_META_KEYWORDS" => "col_keywords",
+			"DETAIL_META_DESCRIPTION" => "col_description",
+			"DETAIL_BROWSER_TITLE" => "col_title",
+			"ACTION_VARIABLE" => "action",
+			"PRODUCT_ID_VARIABLE" => "id",
+			"SECTION_ID_VARIABLE" => "SECTION_ID",
+			"DISPLAY_PANEL" => "N",
+			"CACHE_TYPE" => "N",
+			"CACHE_TIME" => "3600",
+			"CACHE_FILTER" => "N",
+			"CACHE_GROUPS" => "Y",
+			"SET_TITLE" => "Y",
+			"SET_STATUS_404" => "Y",
+			"PRICE_CODE" => array(0=>"col_price",),
+			"USE_PRICE_COUNT" => "N",
+			"SHOW_PRICE_COUNT" => "1",
+			"PRICE_VAT_INCLUDE" => "Y",
+			"PRICE_VAT_SHOW_VALUE" => "N",
+			"LINK_IBLOCK_TYPE" => "",
+			"LINK_IBLOCK_ID" => "",
+			"LINK_PROPERTY_SID" => "",
+			"LINK_ELEMENTS_URL" => "link.php?PARENT_ELEMENT_ID=#ELEMENT_ID#",
+			"DISPLAY_TOP_PAGER" => "N",
+			"DISPLAY_BOTTOM_PAGER" => "N",
+			"DISPLAY_ELEMENT_SELECT_BOX" => "N",
+			"ELEMENT_SORT_FIELD_BOX" => "id",
+			"ELEMENT_SORT_ORDER_BOX" => "asc",
+			"AJAX_OPTION_SHADOW" => "Y",
+			"AJAX_OPTION_JUMP" => "N",
+			"AJAX_OPTION_STYLE" => "Y",
+			"AJAX_OPTION_HISTORY" => "N",
+			"SEF_FOLDER" => "/collection/",
+			"SEF_URL_TEMPLATES" => Array(
+				"section" => "#SECTION_CODE#/",
+				"element" => "#SECTION_CODE#/#ELEMENT_ID#/",
+			),
+			"VARIABLE_ALIASES" => Array(
+				"section" => Array(),
+				"element" => Array(),
+			),
+			"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+			"ADD_SECTIONS_CHAIN" => "N",
+			"JSON" => $json
+		)
+	);
+	if($view_mode != 'search' && empty($url_array[3])) {
+		$dbSec = CIBlockSection::GetList(
+			array(),
+			array(
+				"IBLOCK_ID" => $arParams["IBLOCK_ID"],
+				"CODE" => $url_array[2],
 			)
 		);
-		if(empty($url_array[3]))
-		{
-			$dbSec = CIBlockSection::GetList(
-				array(),
-				array(
-					"IBLOCK_ID" => $arParams["IBLOCK_ID"],
-					"CODE" => $url_array[2],
-				)
-			);
-			if ($arSec = $dbSec->GetNext()) {
+		if ($arSec = $dbSec->GetNext()) {
 ?>
 	<aside class="aside">
 <?
-				$APPLICATION->IncludeComponent(
-					"custom:catalog.section.list",
-					"collection_mainpage",
-					Array(
-						"IBLOCK_TYPE" => "collection",
-						"IBLOCK_ID" => "1",
-						"SECTION_ID" => $arSec["ID"],
-						"DISPLAY_PANEL" => "N",
-						"CACHE_TYPE" => "A",
-						"CACHE_TIME" => "3600",
-						"CACHE_GROUPS" => "Y",
-						"SECTION_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
-						"TOP_DEPTH" => 4,
-						"LEFT_MENU_FLAG" => 1,
-						"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-						"ADD_SECTIONS_CHAIN" => "N"
-					)
-				);
+    $APPLICATION->IncludeComponent(
+			"custom:catalog.section.list",
+			"collection_mainpage",
+			Array(
+				"IBLOCK_TYPE" => "collection",
+				"IBLOCK_ID" => "1",
+				"SECTION_ID" => $arSec["ID"],
+				"DISPLAY_PANEL" => "N",
+				"CACHE_TYPE" => "A",
+				"CACHE_TIME" => "3600",
+				"CACHE_GROUPS" => "Y",
+				"SECTION_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
+				"TOP_DEPTH" => 4,
+				"LEFT_MENU_FLAG" => 1,
+				"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+				"ADD_SECTIONS_CHAIN" => "N"
+			)
+		);
 	$filter_brand =	Array(
 		Array(
 			'LOGIC' => 'OR',
