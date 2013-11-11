fa8e636cf6ddde8c307fc66eaa64614c7e560a9c
shurigo <igor.shurshin@gmail.com>
2013-11-12 02:15:09 +0400

    add .gitignore

--- kohana/create-auth-table.sql
+++ kohana/create-auth-table.sql
@@ -19,7 +19,6 @@ CREATE TABLE IF NOT EXISTS `roles_users` (
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `email` varchar(127) NOT NULL,
-  `username` varchar(32) NOT NULL DEFAULT '',
   `password` varchar(64) NOT NULL,
   `logins` int(10) UNSIGNED NOT NULL DEFAULT '0',
   `last_login` int(10) UNSIGNED,
