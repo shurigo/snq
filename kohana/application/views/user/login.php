f6ba7d3bb6d6b6d52ab78cc7eeef7b98f1c051b8
Slava Levin <vyacheslav.levin@gmail.com>
2013-12-16 23:26:18 +0400

    minor design changes for login page

--- kohana/application/views/user/login.php
+++ kohana/application/views/user/login.php
@@ -1,18 +1,18 @@
-<h2></h2>
+<h2>Войти в Личный Кабинет</h2>
 <? if ($message) : ?>
 	<h3 class="message">
 		<?= $message; ?>
 	</h3>
 <? endif; ?>
-
+<br />
 <?= Form::open('user/login'); ?>
 <p>
-	<?= Form::label('email', 'e-mail'); ?>
+	<?= Form::label('email', 'Адрес электронной почты'); ?><br />
 	<?= Form::input('email', HTML::chars(Arr::get($_POST, 'email'))); ?>
 </p>
 
 <p>
-	<?= Form::label('password', 'Пароль'); ?>
+	<?= Form::label('password', 'Пароль'); ?><br />
 	<?= Form::password('password'); ?>
 </p>
 
@@ -21,9 +21,10 @@
 	<?= Form::label('remember', 'Запомнить меня'); ?>
 </p>
 
-<?= Form::submit('login', 'Вход'); ?>
+<?= Form::submit('login', 'Вход'); ?> <?= Form::submit('register', 'Зарегистрироваться'); ?>
 <?= Form::close(); ?>
-
+<!--
 <p>
 	<?= HTML::anchor('user/create', 'Зарегистрироваться'); ?>
 </p>
+-->
