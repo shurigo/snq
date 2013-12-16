1c16767652cef4a07a0501ce1ea86c6c41777ab9
Slava Levin <vyacheslav.levin@gmail.com>
2013-12-16 21:24:07 +0400

    working kohana version

--- kohana/application/classes/Controller/User.php
+++ kohana/application/classes/Controller/User.php
@@ -8,19 +8,19 @@ class Controller_User extends Controller_Template {
 			->bind('errors', $errors)
 			->bind('message', $message)
 			->bind('user', $user);
-		
+
 		// Load the user information
 		$user = Auth::instance()->get_user();
-		
+
 		// if a user is not logged in, redirect to login page
 		if (!$user)
 		{
 			Request::current()->redirect('user/login');
 		}
 
-		if (HTTP_Request::POST == $this->request->method()) 
-		{			
-			try 
+		if (HTTP_Request::POST == $this->request->method())
+		{
+			try
 			{
 				$user->update_user($_POST, array(
 					'first_name',
@@ -32,16 +32,16 @@ class Controller_User extends Controller_Template {
 				));
 				$_POST = array(); // Reset values so form is not sticky
 				$message = "Данные сохранены";
-				if ($user) 
+				if ($user)
 				{
 					Request::current()->redirect('user/index');
-				} 
-				else 
+				}
+				else
 				{
 					$message = 'Неправильный логин/пароль';
 				}
-			} 
-			catch (ORM_Validation_Exception $e) 
+			}
+			catch (ORM_Validation_Exception $e)
 			{
 				// Set failure message
 				$message = 'Ошибки при сохранении данных';
@@ -52,17 +52,17 @@ class Controller_User extends Controller_Template {
 		}
 	}
 
-	public function action_create() 
+	public function action_create()
 	{
 		$captcha = Captcha::instance();
 		$this->template->content = View::factory('user/create')
 			->bind('captcha', $captcha)
 			->bind('errors', $errors)
 			->bind('message', $message);
-			
-		if (HTTP_Request::POST == $this->request->method()) 
-		{			
-			try 
+
+		if (HTTP_Request::POST == $this->request->method())
+		{
+			try
 			{
 				if(!Captcha::valid($_POST['captcha'])) {
 					$errors['captcha'] = 'Введите правильный код с картинки';
@@ -80,57 +80,86 @@ class Controller_User extends Controller_Template {
 
 				// Grant user login role
 				$user->add('roles', ORM::factory('Role', array('name' => 'login')));
-				
+
 				// Reset values so form is not sticky
 				$_POST = array();
 				Session::instance()->delete('captcha');
-				
+
 				// Set success message
 				$message = "Вы зарегистрировались как '{$user->email}'";
-				
+
 				$this->action_login();
-			} 
-			catch (ORM_Validation_Exception $e) 
+			}
+			catch (ORM_Validation_Exception $e)
 			{
 				// Set failure message
 				$message = 'Ошибки при регистрации';
-				
+
 				// Set errors using custom messages
 				$errors = $e->errors('models');
 			}
 		}
 	}
-	
-	public function action_login() 
+
+	public function action_login()
 	{
 		$this->template->content = View::factory('user/login')
 			->bind('message', $message);
-			
-		if (HTTP_Request::POST == $this->request->method()) 
+
+		if (HTTP_Request::POST != $this->request->method())
 		{
-			// Attempt to login user
-			$remember = array_key_exists('remember', $this->request->post()) ? (bool) $this->request->post('remember') : FALSE;
-			$user = Auth::instance()->login($this->request->post('email'), $this->request->post('password'), $remember);
-			
-			// If successful, redirect user
-			if ($user) 
-			{
-				Request::current()->redirect('user/index');
-			} 
-			else 
-			{
-				$message = 'Неправильный логин/пароль';
-			}
+			return;
+		}
+
+		// Attempt to login user
+		$remember = array_key_exists('remember', $this->request->post()) ? (bool) $this->request->post('remember') : FALSE;
+		$user = Auth::instance()->login($this->request->post('email'), $this->request->post('password'), $remember);
+
+		// If successful, redirect user
+		if ($user)
+		{
+			Request::current()->redirect('user/index');
+		}
+		else
+		{
+			$message = 'Неправильный логин/пароль';
 		}
 	}
-	
-	public function action_logout() 
+
+	public function action_logout()
 	{
 		// Log user out
 		Auth::instance()->logout();
-		
+
 		// Redirect to login page
 		Request::current()->redirect('user/login');
 	}
 
+	public function action_resetpassword()
+	{
+		$this->template->content = View::factory('user/password')
+			->bind('message', $message);
+		if (HTTP_Request::POST != $this->request->method())
+		{
+			return;
+		}
+		$user = ORM::factory('user')
+			->where('username', '=', $this->request->post('username'))
+			->where('email', '=', $this->request->post('email'))
+			->find();
+    if(!$user->loaded())
+		{
+      $message = 'Пользователь не найден';
+      return;
+		}
+    $password_reset = $this->reset_password($user);
+    if($password_reset === TRUE)
+		{
+      $message = 'Письмо с инструкциями по сбросу пароля отправлено на указанный email адрес';
+		}
+		else
+		{
+			$errors = $password_reset;
+		}
+	}
 }
--- urlrewrite.php
+++ urlrewrite.php
@@ -1,41 +1,41 @@
 <?
 $arUrlRewrite = array(
-	array(
-		"CONDITION"	=>	"#^/about/fashion_blog/#",
-		"RULE"	=>	"",
-		"ID"	=>	"bitrix:news",
-		"PATH"	=>	"/about/fashion_blog/index.php",
-	),
-	array(
-		"CONDITION"	=>	"#^/about/about_fur/#",
-		"RULE"	=>	"",
-		"ID"	=>	"bitrix:news",
-		"PATH"	=>	"/about/about_fur/index.php",
-	),
-	array(
-		"CONDITION"	=>	"#^/about/vacancies/#",
-		"RULE"	=>	"",
-		"ID"	=>	"bitrix:iblock.element.add.list",
-		"PATH"	=>	"/about/vacancies/shops.php",
-	),
-	array(
-		"CONDITION"	=>	"#^/actions/#",
-		"RULE"	=>	"",
-		"ID"	=>	"bitrix:news",
-		"PATH"	=>	"/actions/index.php",
-	),
-	array(
-		"CONDITION"	=>	"#^/collection/#",
-		"RULE"	=>	"",
-		"ID"	=>	"custom:catalog",
-		"PATH"	=>	"/collection/index.php",
-	),
+    array(
+        "CONDITION" =>   "#^/about/fashion_blog/#",
+        "RULE"  =>   "",
+        "ID"    =>   "bitrix:news",
+        "PATH"  =>   "/about/fashion_blog/index.php",
+    ),
+    array(
+        "CONDITION" =>   "#^/about/about_fur/#",
+        "RULE"  =>   "",
+        "ID"    =>   "bitrix:news",
+        "PATH"  =>   "/about/about_fur/index.php",
+    ),
+    array(
+        "CONDITION" =>   "#^/about/vacancies/#",
+        "RULE"  =>   "",
+        "ID"    =>   "bitrix:iblock.element.add.list",
+        "PATH"  =>   "/about/vacancies/shops.php",
+    ),
+    array(
+        "CONDITION" =>   "#^/actions/#",
+        "RULE"  =>   "",
+        "ID"    =>   "bitrix:news",
+        "PATH"  =>   "/actions/index.php",
+    ),
+    array(
+        "CONDITION" =>   "#^/collection/#",
+        "RULE"  =>   "",
+        "ID"    =>   "custom:catalog",
+        "PATH"  =>   "/collection/index.php",
+    ),
   array(
     //"CONDITION" => "#^/(user|captcha)/([a-zA-Z]+)/?#",
     "CONDITION" => "#^/(\w+)/(\w+)/?#",
     "RULE" => "$1/$2",
     "PATH" => "/kohana/index.php"
-	),
+    ),
 );
 
 ?>
