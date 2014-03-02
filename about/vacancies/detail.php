aa1a111ec76d0760e5f884aa6c6bdf5a54af61e6
shurigo <igor.shurshin@gmail.com>
2014-03-03 02:45:03 +0400

    bugfix: error when opening a non-existent vacancy by id

--- about/vacancies/detail.php
+++ about/vacancies/detail.php
@@ -8,17 +8,18 @@ $arSelect = Array("ID","NAME","PREVIEW_TEXT","PROPERTY_col_email","PROPERTY_col_
 $arFilter = Array("ID"=>$id);
 $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
 $ob = $res->GetNextElement();
-$arFields = $ob->GetFields();
+if(!$ob):?>
+	<span class="red">Вакансия не найдена</span><?
+else:
+	$arFields = $ob->GetFields();
+	//print
+	echo "<h1>".$arFields['NAME']."</h1>";
+	echo $arFields['PREVIEW_TEXT'];
+	if (strlen($arFields['PROPERTY_COL_PHONE_VALUE'])>0) echo '<br><div><b>Контактный телефон:</b> '.$arFields['PROPERTY_COL_PHONE_VALUE'].'</div>';
+	if (strlen($arFields['PROPERTY_COL_EMAIL_VALUE'])>0) echo '<br><div><b>E-mail:</b> <a href="mailto:'.$arFields['PROPERTY_COL_EMAIL_VALUE'].'">'.$arFields['PROPERTY_COL_EMAIL_VALUE'].'</a></div>';
 
-//print
-echo "<h1>".$arFields['NAME']."</h1>";
-echo $arFields['PREVIEW_TEXT'];
-if (strlen($arFields['PROPERTY_COL_PHONE_VALUE'])>0) echo '<br><div><b>Контактный телефон:</b> '.$arFields['PROPERTY_COL_PHONE_VALUE'].'</div>';
-if (strlen($arFields['PROPERTY_COL_EMAIL_VALUE'])>0) echo '<br><div><b>E-mail:</b> <a href="mailto:'.$arFields['PROPERTY_COL_EMAIL_VALUE'].'">'.$arFields['PROPERTY_COL_EMAIL_VALUE'].'</a></div>';
-
-$vacancy_name=$arFields['NAME'];
-$hr_email=$arFields['PROPERTY_COL_EMAIL_VALUE'];
-include("anketa.php");
-?>
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+	$vacancy_name=$arFields['NAME'];
+	$hr_email=$arFields['PROPERTY_COL_EMAIL_VALUE'];
+	include($_SERVER['DOCUMENT_ROOT'].'/about/vacancies/anketa.php');
+endif;
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
