cead96b1cb706b94adfe1b47da794b474606fcb1
shurigo <igor.shurshin@gmail.com>
2014-03-24 00:38:30 +0400

    extract inactive users by date

--- kohana/application/classes/task/GetPendingActivations.php
+++ kohana/application/classes/task/GetPendingActivations.php
@@ -2,23 +2,42 @@
 /**
  * Output registered but inactive users activation (i.e. activation_date is null) to csv File
  * in the output directory specified by out parameter
+ * @param date: filter inactive users by date (default: today - 1)
  * @param out: output to directory
  */
 class Task_GetPendingActivations extends Minion_Task
 {
-  protected $_options = array('out');
+  protected $_options = array('date', 'out');
 
   protected function _execute(array $params)
   {
+		// Set default dates
+		$in_to   = new DateTime();
+		$in_from = new DateTime(); 
+		$in_from->modify('-1 day');
+		if(isset($params['date']))
+		{
+			$in_to   = DateTime::createFromFormat('Y-m-d', $params['date']);
+			$in_from = DateTime::createFromFormat('Y-m-d', $params['date'])->modify('-1 day');
+		}
+
+		$from = $in_from->format('Y-m-d 23:59:59');
+		$to   = $in_to->format('Y-m-d 23:59:59');
+
+		Minion_CLI::write("Date from:\t$from");
+		Minion_CLI::write("Date to:\t$to");
+
     $users = ORM::factory('User')
       ->where('activation_date', '=', null)
+			->where('registration_date', '>=', $from) 
+			->where('registration_date', '<=', $to) 
       ->find_all();
 
     $cnt = $users->count();
-    Minion_CLI::write("Found $cnt inactive users");
+    Minion_CLI::write("Found $cnt inactive user(s)");
 
     $date = new DateTime();
-    $fname = rtrim($params['out'], '/\\') . DIRECTORY_SEPARATOR . 'inactive_users_' . $date->format('Ymd_His') . '.csv';
+    $fname = rtrim($params['out'], '/\\') . DIRECTORY_SEPARATOR . 'inactive_users_' . $date->format('Ymd') . '.csv';
     Minion_CLI::write("Writing to $fname");
 
     $file = fopen($fname, 'w+');
