e25fc074a294d6b849f07815356be7857c42b94d
shurigo <igor.shurshin@gmail.com>
2014-04-03 14:06:54 +0400

    caption change

--- services/index.php
+++ services/index.php
@@ -7,7 +7,7 @@ $APPLICATION->SetTitle("Услуги");
         <table width="100%" height="450px" border="1" bordercolor="white" bgcolor="#11acdc">
             <tr align="center">
                 <td width="50%"><a href="/services/credit/" style="font-size:22px;color:white;text-decoration: none;">Потребительское кредитование</a></td>
-                <td width="50%"><a href="/services/discount/" style="font-size:22px;color:white;text-decoration: none;">Дисконтная программа</a></td>
+                <td width="50%"><a href="/services/discount/" style="font-size:22px;color:white;text-decoration: none;">Королевский Клуб</a></td>
 
             </tr>
             <tr align="center">
