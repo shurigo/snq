0bdee7afe9b65e6c169e7555cdbd57447a8ad622
Slava Levin <vyacheslav.levin@gmail.com>
2014-04-06 21:40:25 +0400

    minor update for pixels

--- bitrix/components/custom/catalog/templates/.default/bitrix/catalog.element/.default/template.php
+++ bitrix/components/custom/catalog/templates/.default/bitrix/catalog.element/.default/template.php
@@ -19,9 +19,10 @@ false
 function DoubleClickTagPost(tagv){
 var axel = Math.random() + "";
 var a = axel * 10000000000000;
-document.write('<img src="https://ad.doubleclick.net/activity;src=4390744;type=invmedia;cat=' + tagv + ';u1=<?=$url_array[3]?>;u4=<?=$url_array[2]?>;ord=' + a + '?" width="1" height="1" alt=""/>');
+document.write('<img src="https://ad.doubleclick.net/activity;src=4390744;type=invmedia;cat=' + tagv + ';u1=<?=$url_array[3]?>;u4=<?=$url_array[2]?>;ord=' + a + '?" width="1" height="1" alt=""/><noscript><img src="https://ad.doubleclick.net/activity;src=4390744;type=invmedia;cat=' + tagv + ';u1=<?=$url_array[3]?>;u4=<?=$url_array[2]?>;ord=1?" width="1" height="1" alt=""/></noscript>');
 }
 </script>
+
 
 <article class="item" itemscope itemtype="http://schema.org/Product">
   <section class="text">
@@ -342,9 +343,6 @@ document.write('<img src="https://ad.doubleclick.net/activity;src=4390744;type=i
 </script>
 <noscript>
 <img src="https://ad.doubleclick.net/activity;src=4390744;type=invmedia;cat=bosxJSEN;u1=<?=$url_array[3]?>;u4=<?=$url_array[2]?>;ord=1?" width="1" height="1" alt=""/>
-<img src="https://ad.doubleclick.net/activity;src=4390744;type=invmedia;cat=Gobr8Bpp;u1=<?=$url_array[3]?>;u4=<?=$url_array[2]?>;ord=1?" width="1" height="1" alt=""/>
-<img src="https://ad.doubleclick.net/activity;src=4390744;type=invmedia;cat=qil7ECb1;u1=<?=$url_array[3]?>;u4=<?=$url_array[2]?>;ord=1?" width="1" height="1" alt=""/>
-
 </noscript>
 <!-- End of DoubleClick Floodlight Tag: Please do not remove -->
 <script>
