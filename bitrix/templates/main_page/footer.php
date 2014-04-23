26359d0e155bc79e1a13b9b6acab7f9d010ff307
Slava Levin <vyacheslav.levin@gmail.com>
2014-04-24 00:00:50 +0400

    New pixels for Adclock

--- bitrix/templates/main_page/footer.php
+++ bitrix/templates/main_page/footer.php
@@ -212,6 +212,23 @@ var google_remarketing_only = true;
 </div>
 </noscript>
 
+
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 974437234;
+var google_conversion_label = "KGI-COaqlwgQ8vbS0AM";
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/974437234/?value=1.000000&amp;label=KGI-COaqlwgQ8vbS0AM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
 <!--
 Start of DoubleClick Floodlight Tag: Please do not remove
 Activity name of this tag: RU - Snowqueen - Home - 2014 - RT
