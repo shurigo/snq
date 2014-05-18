46f1fd59a422176cae3b04bf3187c71c35e12521
shurigo <igor.shurshin@gmail.com>
2014-05-19 00:27:36 +0400

    get new users job: date interval corrected

--- kohana/application/classes/task/Getinactiveusers.php
+++ kohana/application/classes/task/Getinactiveusers.php
@@ -21,7 +21,7 @@ class Task_Getinactiveusers extends Minion_Task
 			$in_from = DateTime::createFromFormat('Y-m-d', $params['date'])->modify('-1 day');
 		}
 
-		$from = $in_from->format('Y-m-d 23:59:59');
+		$from = $in_from->format('Y-m-d 00:00:00');
 		$to   = $in_to->format('Y-m-d 23:59:59');
 
 		Minion_CLI::write("Date from:\t$from");
