d36171c3a2f9f32d2340ef306730cc8048bd30b9
Slava Levin <vyacheslav.levin@gmail.com>
2014-08-14 21:35:34 +0400

    change service block

--- services/index.php
+++ services/index.php
@@ -6,16 +6,16 @@ $APPLICATION->SetTitle("");
         <table width="100%" border="0" bgcolor="#ffffff">
             <tr align="center">
                 <td width="50%"><a href="/services/discount/"><img src="/images/1-01.jpg" width="498px" height="282px" alt="Королевский Клуб" border="0"></a></td>
-                <td width="50%"><a href="/services/credit/"><img src="/images/1-03.jpg" alt="Потребительское кредитование" width="499px" height="282px" border="0"></a></td>
+                <td width="50%"><a href="/actions/37279186/"><img src="/images/1-09.jpg" alt="Кредит без переплат" width="498px" height="282px" border="0"></a></td>
 
             </tr>
             <tr align="center">
-                <td width="50%"><a href="/services/cards/"><img src="/images/1-02.jpg" alt="Подарочные карты" width="499px" height="282px" border="0"></a></td>
-                <td width="50%"><a href="/services/megacard/"><img src="/images/1-05.jpg" width="498px" height="282px" alt="Мы принимаем к оплате MEGACARD" border="0"></a></td>
+                <td width="50%"><a href="/services/credit/"><img src="/images/1-03.jpg" alt="Потребительское кредитование" width="498px" height="282px" border="0"></a></td>
+                <td width="50%"><a href="/services/cards/"><img src="/images/1-08.jpg" alt="Подарочные карты" width="499px" height="282px" border="0"></a></td>
             </tr>
              <tr align="center">
-                <td width="50%"><a href="/actions/22700630/"><img src="/images/1-04.jpg" alt="Кредитная карта Снежная Королева" width="499px" height="282px" border="0"></a></td>
-                <td width="50%"><a href="/services/atele/"><img src="/images/1-06.jpg" width="498px" height="282px" alt="Услуги ателье" border="0"></a></td>
+                 <td width="50%"><a href="/services/megacard/"><img src="/images/1-05.jpg" width="498px" height="282px" alt="Мы принимаем к оплате MEGACARD" border="0"></a></td>
+                 <td width="50%"><a href="/services/atele/"><img src="/images/1-06.jpg" width="498px" height="282px" alt="Услуги ателье" border="0"></a></td>
             </tr>
         </table>
 
