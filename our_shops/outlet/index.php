c66a0a9e39650e98c228bb5dcf3ea852e02288ad
Slava Levin <vyacheslav.levin@gmail.com>
2014-10-01 21:56:39 +0400

    add outlet page

---
+++ our_shops/outlet/index.php
@@ -0,0 +1,21 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Дисконт магазины");
+?>
+<section class="mainContent2">
+<h1>Дисконт магазины</h1>
+         <p>
+         <b>Москва:</b><br />
+         м.«Братиславская», ул. Люблинская, д. 153, ТЦ «Л-153»<br />
+         пос. Московский, дер. Лапшинка, вл. 8, корп. 1, «Vnukovo Outlet Village»<br /><br />
+
+         <b>Санкт-Петербург:</b><br />
+         м. «Проспект Просвещения», пр-т Энгельса, д. 154, ТЦ «Гранд-Каньон»<br /><br />
+
+         <b>Краснодар:</b>
+         ул. Стасова/Сормовская, 178 – 180/1 ТЦ «Галактика»
+         </p>
+ </section>
+
+
+ <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
