27fd2d829d7f3bceb10b883a5ef07a4cbbc13e6b
Slava Levin <vyacheslav.levin@gmail.com>
2014-10-26 21:00:46 +0300

    remove brand name from brand page - leabe only image

--- bitrix/templates/inside_pages/components/bitrix/news/brands/bitrix/news.detail/.default/template.php
+++ bitrix/templates/inside_pages/components/bitrix/news/brands/bitrix/news.detail/.default/template.php
@@ -5,10 +5,11 @@
 ?>
 
 <div class="news-detail">
+	<!--
 	<?if($arParams["DISPLAY_NAME"]!="N" && $arResult["NAME"]):?>
 		<h1><?=$arResult["NAME"]?></h1>
 	<?endif;?>
-
+    -->
 	<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
 		<img class="detail_picture" border="0" src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>" width="<?=$arResult["DETAIL_PICTURE"]["WIDTH"]?>" height="<?=$arResult["DETAIL_PICTURE"]["HEIGHT"]?>" alt="<?=$arResult["NAME"]?>"  title="<?=$arResult["NAME"]?>" />
 	<?endif?>
