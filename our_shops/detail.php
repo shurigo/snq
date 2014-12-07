f848502d99b1ec57063df6fc26b78569b7fd53d6
shurigo <igor.shurshin@gmail.com>
2014-12-08 00:26:27 +0300

    added more fields to the shop detail page

--- our_shops/detail.php
+++ our_shops/detail.php
@@ -19,10 +19,43 @@
 			LocalRedirect('/our_shops', true);
 		} else { // print the shop's details
 			$fields = $el_shop->GetFields();
+			$props = $el_shop->GetProperties();
 			$APPLICATION->SetTitle($fields['NAME'].'. '.$fields['PREVIEW_TEXT']);
 ?>
 			<h1><?=$fields['NAME']?></h1>
-			<div id="description"><?=$fields['PREVIEW_TEXT']?>
+			<?if(!empty($props['PREVIEW_TEXT']['VALUE'])):?>
+				<div id="description"><?=$props['PREVIEW_TEXT']['VALUE']?>
+			<?endif;?>
+			<?if(!empty($props['col_title']['VALUE'])):?>
+				<div id="title"><?=$props['col_title']['VALUE']?>
+			<?endif;?>
+			<?if(!empty($props['col_waymark']['VALUE'])):?>
+				<div id="waymark"><?=$props['col_waymark']['VALUE']?>
+			<?endif;?>
+			<?if(!empty($props['col_phone']['VALUE'])):?>
+				<div id="phone"><?=$props['col_phone']['VALUE']?>
+			<?endif;?>
+			<?if(!empty($props['col_web']['VALUE'])):?>
+				<div id="web"><?=$props['col_web']['VALUE']?>
+			<?endif;?>
+			<?if(!empty($props['col_address']['VALUE'])):?>
+				<div id="address"><?=$props['col_address']['VALUE']?>
+			<?endif;?>
+			<?if(!empty($props['col_subway_station']['VALUE'])):?>
+				<div id="subway_station"><?=$props['col_subway_station']['VALUE']?>
+			<?endif;?>
+			<?if(!empty($props['col_schedule']['VALUE'])):?>
+				<div id="schedule"><?=$props['col_schedule']['VALUE']?>
+			<?endif;?>
+			<?if(!empty($props['col_shuttle']['VALUE'])):?>
+				<div id="shuttle"><?=$props['col_shuttle']['VALUE']?>
+			<?endif;?>
+			<?if(!empty($props['col_transport']['VALUE'])):?>
+				<div id="transport"><?=$props['col_transport']['VALUE']?>
+			<?endif;?>
+			<?if(!empty($props['col_atelier']['VALUE'])):?>
+				<div id="atelier"><?=$props['col_atelier']['VALUE']?>
+			<?endif;?>
 			<div id="ymaps-map-id_133707667107155769727" style="width: 998px; height: 400px;"></div>
 			<div style="width: 998px; text-align: right;"><a href="http://api.yandex.ru/maps/tools/constructor/?lang=ru-RU" target="_blank" style="color: #191a1e; font: 13px Arial,Helvetica,sans-serif;"></a></div>
 			<script type="text/javascript" src="http://api-maps.yandex.ru/2.0/?coordorder=longlat&load=package.full&wizard=constructor&lang=ru-RU&onload=fid_133707667107155769727"></script>
